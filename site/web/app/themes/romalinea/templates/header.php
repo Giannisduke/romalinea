d2333a66841a7d2848d30136950ca3ca11baed6c
Giannisduke <admin@eboy.gr>
2021-06-20 22:52:03 +0300

    Header with collapse icon

--- site/web/app/themes/romalinea/templates/header.php
+++ site/web/app/themes/romalinea/templates/header.php
@@ -12,12 +12,11 @@
     </div>
     </div>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#demo" aria-controls="demo" aria-expanded="false" aria-label="Toggle navigation">
-     <span class="icon-bar top-bar"></span>
-     <span class="icon-bar middle-bar"></span>
-     <span class="icon-bar bottom-bar"></span>
-     <span class="sr-only">Toggle navigation</span>
-   </button>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#demo" aria-controls="demo" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="icon-bar top-bar"></span>
+      <span class="icon-bar middle-bar"></span>
+      <span class="icon-bar bottom-bar"></span>
+    </button>
 
     <?php //wp_nav_menu( array( 'theme_location'    => 'top_left' ) ); ?>
     <?php do_action( 'roma_header_form' ); ?>
