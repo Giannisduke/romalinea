381ccf8ec6c58f7dfbf871afe8714bdf981538d2
Giannisduke <admin@eboy.gr>
2021-06-20 17:25:18 +0300

    fix remove left menu

--- site/web/app/themes/romalinea/templates/header.php
+++ site/web/app/themes/romalinea/templates/header.php
@@ -5,15 +5,6 @@
         <a href="<?php echo home_url(); ?>">
       <img class="logo" src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
         </a>
-      <?php wp_nav_menu( array(
-        'theme_location'    => 'top_left',
-        'menu_class'           => 'menu top-left order-md-2 ml-auto',
-        'items_wrap'           => '<ul class="%2$s">%3$s</ul>',
-        'item_separator' => '&middot;',
-        //'menu_id'   => 'menu_id',
-        'link_before' => '<span class="text">',
-        'link_after' => '</span>'
-       ) ); ?>
       <div>
       <?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>
       <h1><?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?></h1>
