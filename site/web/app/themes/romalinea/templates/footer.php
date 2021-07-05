761e79d5c0b19a9707205f2a3f5effa342edb694
Giannisduke <admin@eboy.gr>
2021-07-05 18:32:17 +0300

    hero responsive

--- site/web/app/themes/romalinea/templates/footer.php
+++ site/web/app/themes/romalinea/templates/footer.php
@@ -18,15 +18,55 @@
     </div>
     </div>
     <div class="col-2">
+      <?php $menu_footer_1 = get_term(get_nav_menu_locations()['footer_nav_1'], 'nav_menu')->name; ?>
+      <?php $menu_footer_2 = get_term(get_nav_menu_locations()['footer_nav_2'], 'nav_menu')->name; ?>
 
+      <?php
+      wp_nav_menu( array(
+      'theme_location'  => 'footer_nav_1',
+      'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+      'container'       => false,
+      'container_class' => '',
+      'container_id'    => '',
+      'menu_class'      => 'nav flex-column',
+      'items_wrap'      => '<nav id="%1$s" class="%2$s">%3$s</nav>',
+      'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+      'walker'          => new WP_Bootstrap_Navwalker(),
+  ) );
+  ?>
     </div>
     <div class="col-2">
-
+    
+      <?php
+      wp_nav_menu( array(
+      'theme_location'  => 'footer_nav_2',
+      'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+      'container'       => false,
+      'container_class' => '',
+      'container_id'    => '',
+      'menu_class'      => 'nav flex-column',
+      'items_wrap'      => '<nav id="%1$s" class="%2$s">%3$s</nav>',
+      'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+      'walker'          => new WP_Bootstrap_Navwalker(),
+  ) );
+  ?>
     </div>
     <div class="col-4">
       <div class="d-flex align-items-end flex-column mb-3" style="height: 200px;">
     <div class="p-2">
-
+      <?php
+      wp_nav_menu( array(
+      'theme_location'  => 'footer_social',
+      'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+      'container'       => false,
+      'container_class' => '',
+      'container_id'    => '',
+      'menu_class'      => 'nav',
+      'items_wrap'      => '<nav id="%1$s" class="%2$s">%3$s</nav>',
+      'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+      'walker'          => new WP_Bootstrap_Navwalker(),
+  ) );
+  ?>
     </div>
     <div class="mt-auto p-2 bd-highlight"></div>
   </div>
